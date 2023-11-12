e57f6614ca4795dc18ed19801d6d3047815abf7b
guitomesz <42675646+Guitomesz@users.noreply.github.com>
2023-11-12 11:30:17 -0300

    Update - arrumei identação

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -4,7 +4,6 @@ public class Calculadora {
         int vitorias = 110;
         int derrotas = 10;
         int saldoVitorias = Calculos.saldoVitorias(vitorias, derrotas);
-
         String nivel = Calculos.nivelHeroi(saldoVitorias);
 
         System.out.println("O Herói tem um saldo de " + saldoVitorias + " e está no nível de " + nivel);
